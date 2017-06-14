85072807999f268f7bbfcd758a089691c61a2306
HouJP <houjp1992@gmail.com>
2017-06-14 10:41:51 +0800

    add btm_features.save_question_topic_info

---
+++ bin/btm_features.py
@@ -0,0 +1,43 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/6/13 23:11
+# @Author  : HouJP
+# @Email   : houjp1992@gmail.com
+
+
+import ConfigParser
+import data_utils
+
+
+def save_question_topic_info(cf):
+    q_train_set = cf.get('DEFAULT', 'source_pt') + '/question_train_set.txt'
+    (qid_train_list, tc_train_list, tw_train_list, dc_train_list, dw_train_list) = data_utils.load_question_set(q_train_set)
+
+    q_eval_set = cf.get('DEFAULT', 'source_pt') + '/question_eval_set.txt'
+    (qid_eval_list, tc_eval_list, tw_eval_list, dc_eval_list, dw_eval_list) = data_utils.load_question_set(q_eval_set)
+
+    q_topic_set = cf.get('DEFAULT', 'source_pt') + '/topic_info.txt.small'
+    (tid_topic_list, father_topic_list, tc_topic_list, tw_topic_list, dc_topic_list, dw_topic_list) = data_utils.load_topic_info(q_topic_set)
+
+    btm_qt_info_fp = cf.get('DEFAULT', 'devel_pt') + '/btm_qt_info.txt'
+    f = open(btm_qt_info_fp, 'w')
+    for i in range(len(qid_train_list)):
+        f.write(' '.join((tw_train_list[i] + dw_train_list[i])) + '\n')
+    for i in range(len(qid_eval_list)):
+        f.write(' '.join((tw_eval_list[i] + dw_eval_list[i])) + '\n')
+    for i in range(len(tid_topic_list)):
+        f.write(' '.join((tw_topic_list[i] + dw_topic_list[i])) + '\n')
+    f.close()
+
+
+def main():
+    conf_fp = '/home/houjianpeng/zhihu-machine-learning-challenge-2017/conf/default.conf'
+    cf = ConfigParser.ConfigParser()
+    cf.read(conf_fp)
+
+    save_question_topic_info(cf)
+
+
+if __name__ == '__main__':
+    main()
+
--- bin/data_utils.py
+++ bin/data_utils.py
@@ -43,7 +43,42 @@ def load_question_set(fp):
     return qid_list, tc_list, tw_list, dc_list, dw_list
 
 
-def _test_load_question_set():
+def load_topic_info(fp):
+    f = open(fp)
+    tid_list = []
+    father_list = []
+    tc_list = []
+    tw_list = []
+    dc_list = []
+    dw_list = []
+    for line in f:
+        subs = line.strip().split('\t')
+        tid_list.append(subs[0])
+        if 1 < len(subs):
+            father_list.append(subs[1].split(','))
+        else:
+            father_list.append([])
+        if 2 < len(subs):
+            tc_list.append(subs[2].split(','))
+        else:
+            tc_list.append([])
+        if 3 < len(subs):
+            tw_list.append(subs[3].split(','))
+        else:
+            tw_list.append([])
+        if 4 < len(subs):
+            dc_list.append(subs[4].split(','))
+        else:
+            dc_list.append([])
+        if 5 < len(subs):
+            dw_list.append(subs[5].split(','))
+        else:
+            dw_list.append([])
+    f.close()
+    return tid_list, father_list, tc_list, tw_list, dc_list, dw_list
+
+
+def _test_load_question_set(cf):
     q_train_set = cf.get('DEFAULT', 'source_pt') + '/question_train_set.txt.small'
 
     (qid_list, tc_list, tw_list, dc_list, dw_list) = load_question_set(q_train_set)
@@ -53,9 +88,31 @@ def _test_load_question_set():
     print dc_list
     print dw_list
 
-if __name__ == '__main__':
+
+def _test_load_topic_info(cf):
+    q_topic_set = cf.get('DEFAULT', 'source_pt') + '/topic_info.txt.small'
+
+    (tid_list, father_list, tc_list, tw_list, dc_list, dw_list) = load_topic_info(q_topic_set)
+    print tid_list
+    print father_list
+    print tc_list
+    print tw_list
+    print dc_list
+    print dw_list
+
+
+def _test():
     conf_fp = '/Users/houjianpeng/Github/zhihu-machine-learning-challenge-2017/conf/default.conf'
     cf = ConfigParser.ConfigParser()
     cf.read(conf_fp)
 
-    _test_load_question_set()
+    # _test_load_question_set(cf)
+    _test_load_topic_info(cf)
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    _test()
+    # main()

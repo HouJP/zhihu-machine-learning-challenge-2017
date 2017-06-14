6f8317e057e355a9334a547b1873fd0d675bb6c4
HouJP <houjp1992@gmail.com>
2017-06-14 14:17:20 +0800

    add topic_analysis.count_topic

---
+++ bin/topic_analysis.py
@@ -0,0 +1,15 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/6/14 14:13
+# @Author  : HouJP
+# @Email   : houjp1992@gmail.com
+
+
+def count_topic(topic_mat):
+    topic_cnt = {}
+    for vec in topic_mat:
+        for tid in vec:
+            topic_cnt[tid] = topic_cnt.get(tid, 0.)
+    return topic_cnt
+
+

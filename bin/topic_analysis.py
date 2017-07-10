a380b5572dd2ff1863409a9ebd47adcd783ee4ac
HouJP <houjp1992@gmail.com>
2017-07-11 02:07:15 +0800

    add plot_lid_num

--- bin/topic_analysis.py
+++ bin/topic_analysis.py
@@ -5,6 +5,11 @@
 # @Email   : houjp1992@gmail.com
 
 
+from text_cnn.data_helpers import load_lid
+import sys
+import ConfigParser
+
+
 def count_topic(topic_mat):
     topic_cnt = {}
     for vec in topic_mat:
@@ -13,5 +18,33 @@ def count_topic(topic_mat):
     return topic_cnt
 
 
+def plot_lid_num(config):
+    lid_fp = '%s/label_id.offline.csv' % config.get('DIRECTORY', 'dataset_pt')
+    lid = load_lid(lid_fp, 1999)
 
+    num = []
+    tol = 0
+    spe_lid = 99
+    for i in range(len(lid)):
+        if 0 == i % 10000:
+            num.append(tol)
+        if 1 == lid[i][spe_lid]:
+            tol += 1
+
+    print num
+
+
+def main():
+    config_fp = sys.argv[1]
+    config = ConfigParser.ConfigParser()
+    config.read(config_fp)
+
+    func = sys.argv[2]
+
+    eval(func)(config)
+
+
+if __name__ == '__main__':
+    # _test()
+    main()
 

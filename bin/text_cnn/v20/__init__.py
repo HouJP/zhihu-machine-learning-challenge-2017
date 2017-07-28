a3f6d4eaccb9f1297b4dc06099a27c87794d3a4d
HouJP <houjp1992@gmail.com>
2017-07-28 22:58:37 +0800

    add v21

--- bin/text_cnn/v20/__init__.py
+++ bin/text_cnn/v20/__init__.py
@@ -3,3 +3,10 @@
 # @Time    : 2017/7/28 17:27
 # @Author  : HouJP
 # @Email   : houjp1992@gmail.com
+
+
+"""
+v20:
+    1. add L2 for v19
+    2. change learn rate => 0.00005
+"""
---
+++ bin/text_cnn/v21/__init__.py
@@ -0,0 +1,5 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/7/28 22:57
+# @Author  : HouJP
+# @Email   : houjp1992@gmail.com

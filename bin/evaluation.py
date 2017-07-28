4fce4a72a9fa69bd82c87e15c0b6d0e93c0cfa75
HouJP <houjp1992@gmail.com>
2017-07-28 12:01:36 +0800

    update evaluation

--- bin/evaluation.py
+++ bin/evaluation.py
@@ -49,3 +49,41 @@ def F(preds, labels):
     LogUtil.log('INFO', 'precision=%s, recall=%s, f=%s' % (str(precision),
                                                            str(recall),
                                                            str((precision * recall) / (precision + recall))))
+
+
+def F_new(preds, labels):
+    topk = 5
+
+    right_label_num = 0
+    right_label_at_pos_num = [0] * 5
+    sample_num = 0
+    all_marked_label_num = 0
+
+    for i, ps in enumerate(preds):
+        sample_num += 1
+        top5 = enumerate(ps)
+        top5 = sorted(top5, key=lambda s:s[1], reverse=True)
+        top5_ids = [x[0] for x in top5[:5]]
+
+        label_ids = list()
+        for kv in enumerate(labels[i]):
+            if 1 == kv[1]:
+                label_ids.append(kv[0])
+
+        marked_label_set = set(label_ids)
+        all_marked_label_num += len(marked_label_set)
+
+        for pos, label in enumerate(top5_ids):
+            if label in marked_label_set:
+                right_label_num += 1
+                right_label_at_pos_num[pos] += 1
+
+    precision = 0.0
+
+    for pos, right_num in zip(range(0, topk), right_label_at_pos_num):
+        precision += (right_num / float(sample_num)) / math.log(2.0 + pos)
+    recall = float(right_label_num) / all_marked_label_num
+
+    LogUtil.log('INFO', 'precision=%s, recall=%s, f=%s' % (str(precision),
+                                                           str(recall),
+                                                           str((precision * recall) / (precision + recall))))

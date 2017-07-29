95bac34358dd8df5bea3afaca864708df3e09fae
HouJP <houjp1992@gmail.com>
2017-07-30 00:27:09 +0800

    add dmatrix.generate

---
+++ bin/rank/dmatrix.py
@@ -0,0 +1,64 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/7/30 00:04
+# @Author  : HouJP
+# @Email   : houjp1992@gmail.com
+
+
+import sys
+import re
+import ConfigParser
+
+from ..text_cnn.data_helpers import parse_feature_vec
+
+
+def generate(config, argv):
+    data_name = argv[0]
+    # load rank features
+    feature_names = config.get('RANK', 'rank_features').split()
+    feature_files = [open('%s/rank_%s.%s.csv' % (config.get('DIRECTORY', 'dataset_pt'), fn, data_name), 'r') for fn in feature_names]
+
+    # load rank labels
+    label_file = open('%s/rank_%s.%s.csv' % (config.get('DIRECTORY', 'dataset_pt'), 'labels', data_name), 'r')
+
+    dmatrix_file = open('%s/rank_%s.%s.csv' % (config.get('DIRECTORY', 'dataset_pt'), 'dmatrix', data_name), 'w')
+    group_file = open('%s/rank_%s.%s.csv' % (config.get('DIRECTORY', 'dataset_pt'), 'group', data_name), 'w')
+
+    while True:
+        features = list()
+        end_while = False
+        for feature_file in feature_files:
+            line = feature_file.readline()
+            if '' == line:
+                end_while = True
+                break
+            features.append(parse_feature_vec(line))
+        if end_while:
+            break
+
+        line = label_file.readline()
+        label = [int(num) for num in re.split(' |,', line.strip('\n'))]
+
+        for i in range(len(label)):
+            vec = list()
+            for j in range(len(features)):
+                vec.append(features[j][i])
+            s = ' '.join(['%d:%s' % (kv[0], kv[1]) for kv in enumerate(vec)])
+            dmatrix_file.write('%d %s\n' % (label[i], s))
+        group_file.write('%d\n' % len(label))
+
+    for f in feature_files:
+        f.close()
+    label_file.close()
+    dmatrix_file.close()
+    group_file.close()
+
+
+if __name__ == '__main__':
+    config_fp = sys.argv[1]
+    config = ConfigParser.ConfigParser()
+    config.read(config_fp)
+    func = sys.argv[2]
+    argv = sys.argv[3:]
+
+    eval(func)(config, argv)

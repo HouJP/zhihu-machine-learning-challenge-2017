99af55acd652c613395ccfa952101bae35cd6b8b
HouJP <houjp1992@gmail.com>
2017-07-30 11:37:34 +0800

    add model.train_test

--- bin/rank/model.py
+++ bin/rank/model.py
@@ -8,6 +8,7 @@
 import xgboost as xgb
 import sys
 import ConfigParser
+import json
 from ..utils import DataUtil, LogUtil
 from ..text_cnn.data_helpers import load_labels_from_file
 from ..evaluation import F_by_ids
@@ -84,6 +85,64 @@ def train(config, argv):
 
     F_by_ids(preds_ids, valid_labels)
 
+
+def train_online(config, argv):
+    dtrain_fp = stand_path('%s/rank_%s.%s.csv' % (config.get('DIRECTORY', 'dataset_pt'), 'dmatrix', 'offline'))
+    group_train_fp = '%s/rank_%s.%s.csv' % (config.get('DIRECTORY', 'dataset_pt'), 'group', 'offline')
+    dtrain = xgb.DMatrix(dtrain_fp)
+    dtrain.set_group(DataUtil.load_vector(group_train_fp, 'int'))
+
+    dtest_fp = stand_path('%s/rank_%s.%s.csv' % (config.get('DIRECTORY', 'dataset_pt'), 'dmatrix', 'online'))
+    group_test_fp = '%s/rank_%s.%s.csv' % (config.get('DIRECTORY', 'dataset_pt'), 'group', 'online')
+    dtest = xgb.DMatrix(dtest_fp)
+    dtest.set_group(DataUtil.load_vector(group_test_fp, 'int'))
+
+    params = load_parameters(config)
+    model = xgb.train(params,
+                      dtrain,
+                      params['num_round'],
+                      early_stopping_rounds=params['early_stop'],
+                      verbose_eval=params['verbose_eval'])
+
+    # make prediction
+    topk = config.getint('RANK', 'topk')
+    test_preds = model.predict(dtest, ntree_limit=model.best_ntree_limit)
+    test_preds = [num for num in test_preds]
+    test_preds = zip(*[iter(test_preds)] * topk)
+
+    # load topk ids
+    index_pt = config.get('DIRECTORY', 'index_pt')
+    topk_class_index_fp = '%s/%s.%s.index' % (index_pt, config.get('RANK', 'topk_class_index'), 'online')
+    topk_label_id = DataUtil.load_matrix(topk_class_index_fp, 'int')
+
+    preds_ids = list()
+    for i in range(5000):
+        preds_ids.append([kv[0] for kv in sorted(zip(topk_label_id[i], test_preds[i]), key=lambda x:x[1], reverse=True)])
+
+    # load question ID for online dataset
+    qid_on_fp = '%s/%s.online.csv' % (config.get('DIRECTORY', 'dataset_pt'), 'question_id')
+    qid_on = DataUtil.load_vector(qid_on_fp, 'str')
+    LogUtil.log('INFO', 'load online question ID done')
+
+    # load hash table of label
+    id2label_fp = '%s/%s' % (config.get('DIRECTORY', 'hash_pt'), config.get('TITLE_CONTENT_CNN', 'id2label_fn'))
+    id2label = json.load(open(id2label_fp, 'r'))
+
+    run_id = 2
+    rank_submit_fp = '%s/rank_submit.online.%02d' % (config.get('DIRECTORY', 'tmp_pt'), run_id)
+    rank_submit_f = open(rank_submit_fp, 'w')
+    for line_id, p in enumerate(preds_ids):
+        label_sorted = [id2label[str(kv[0])] for kv in p[:5]]
+        rank_submit_f.write("%s,%s\n" % (qid_on[line_id], ','.join(label_sorted)))
+        if 0 == line_id % 10000:
+            LogUtil.log('INFO', '%d lines prediction done' % line_id)
+    rank_submit_f.close()
+
+    rank_submit_fp = '%s/rank_all.online.%02d' % (config.get('DIRECTORY', 'tmp_pt'), run_id)
+    rank_submit_f = open(rank_submit_fp, 'w')
+    for p in test_preds:
+        rank_submit_f.write('%s\n' % ','.join([str(num) for num in p]))
+    rank_submit_f.close()
 
 
 if __name__ == '__main__':

9fd79e9c07e6072251ed0b73bc0b672e535c9e9a
HouJP <houjp1992@gmail.com>
2017-08-02 22:26:05 +0800

    update btm_dis_features

--- bin/btm_dis_features.py
+++ bin/btm_dis_features.py
@@ -12,7 +12,8 @@ import json
 import sys
 import ConfigParser
 from data_utils import load_topic_info
-from text_cnn.data_helpers import parse_feature_vec
+from text_cnn.data_helpers import parse_feature_vec, load_features_from_file
+from utils import DataUtil, LogUtil
 
 
 def load_topic_btm_vec(config):
@@ -38,14 +39,17 @@ def load_topic_btm_vec(config):
 
 
 def generate(config, argv):
+    # load valid dataset index
+    valid_index_fp = '%s/%s.offline.index' % (config.get('DIRECTORY', 'index_pt'),
+                                              config.get('TITLE_CONTENT_CNN', 'valid_index_offline_fn'))
+    valid_index = DataUtil.load_vector(valid_index_fp, 'int')
+    valid_index = [num - 1 for num in valid_index]
+
     # load topic btm vec
     topic_btm_vec = load_topic_btm_vec(config)
 
     # offline / online
     data_name = argv[0]
-
-    btm_vec_fp = '%s/fs_btm_tw_cw.%s.csv' % (config.get('DIRECTORY', 'dataset_pt'), data_name)
-    btm_vec_f = open(btm_vec_fp, 'r')
 
     dis_func_names = ["cosine",
                       "cityblock",
@@ -60,17 +64,34 @@ def generate(config, argv):
                                                 fn,
                                                 data_name), 'w') for fn in btm_dis_feature_fn]
 
-    for line in btm_vec_f:
-        doc_vec = np.nan_to_num(parse_feature_vec(line))
-        for dis_id in range(len(dis_func_names)):
-            vec = [0.] * 1999
-            for topic_id in range(1999):
-                topic_vec = topic_btm_vec[topic_id]
-                if 'minkowski' == dis_func_names[dis_id]:
-                    vec[topic_id] = eval(dis_func_names[dis_id])(doc_vec, topic_vec, 3)
-                else:
-                    vec[topic_id] = eval(dis_func_names[dis_id])(doc_vec, topic_vec)
-            btm_dis_feature_f[dis_id].write('%s\n' % ','.join([str(num) for num in vec]))
+    if 'offline' == data_name:
+        btm_tw_cw_features = load_features_from_file(config, 'fs_btm_tw_cw', data_name, valid_index)
+        LogUtil.log('INFO', 'load_features_from_file, len=%d' % len(btm_tw_cw_features))
+        for line_id in range(len(btm_tw_cw_features)):
+            doc_vec = btm_tw_cw_features[line_id]
+            for dis_id in range(len(dis_func_names)):
+                vec = [0.] * 1999
+                for topic_id in range(1999):
+                    topic_vec = topic_btm_vec[topic_id]
+                    if 'minkowski' == dis_func_names[dis_id]:
+                        vec[topic_id] = eval(dis_func_names[dis_id])(doc_vec, topic_vec, 3)
+                    else:
+                        vec[topic_id] = eval(dis_func_names[dis_id])(doc_vec, topic_vec)
+                btm_dis_feature_f[dis_id].write('%s\n' % ','.join([str(num) for num in vec]))
+    else:
+        btm_vec_fp = '%s/fs_btm_tw_cw.%s.csv' % (config.get('DIRECTORY', 'dataset_pt'), data_name)
+        btm_vec_f = open(btm_vec_fp, 'r')
+        for line in btm_vec_f:
+            doc_vec = np.nan_to_num(parse_feature_vec(line))
+            for dis_id in range(len(dis_func_names)):
+                vec = [0.] * 1999
+                for topic_id in range(1999):
+                    topic_vec = topic_btm_vec[topic_id]
+                    if 'minkowski' == dis_func_names[dis_id]:
+                        vec[topic_id] = eval(dis_func_names[dis_id])(doc_vec, topic_vec, 3)
+                    else:
+                        vec[topic_id] = eval(dis_func_names[dis_id])(doc_vec, topic_vec)
+                btm_dis_feature_f[dis_id].write('%s\n' % ','.join([str(num) for num in vec]))
 
     for f in btm_dis_feature_f:
         f.close()

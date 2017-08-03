0e41a2f2204f7bfb814e1068272c10914ea08b6f
HouJP <houjp1992@gmail.com>
2017-08-03 22:07:40 +0800

    update feature

--- bin/rank/feature.py
+++ bin/rank/feature.py
@@ -7,48 +7,48 @@
 
 import sys
 import ConfigParser
+import hashlib
 from ..utils import DataUtil, LogUtil
 from os.path import isfile
 from ..text_cnn.data_helpers import load_features_from_file
+from ..featwheel.feature import Feature
 
 
-def generate(config, argv):
+def generate_featwheel_feature(config, argv):
     data_name = argv[0]
-    # load valid dataset index
-    valid_index_fp = '%s/%s.offline.index' % (config.get('DIRECTORY', 'index_pt'),
-                                                  config.get('TITLE_CONTENT_CNN', 'valid_index_offline_fn'))
-    valid_index = DataUtil.load_vector(valid_index_fp, 'int')
-    valid_index = [num - 1 for num in valid_index]
+
+    topk = config.getint('RANK', 'topk')
 
     # load topk ids
     index_pt = config.get('DIRECTORY', 'index_pt')
-    topk_class_index_fp = '%s/%s.%s.index' % (index_pt, config.get('RANK', 'topk_class_index'), data_name)
-    topk_label_id = DataUtil.load_matrix(topk_class_index_fp, 'int')
+    vote_feature_names = config.get('RANK', 'vote_features').split()
+    topk_label_file_name = hashlib.md5('|'.join(vote_feature_names)).hexdigest()
+    topk_label_file_path = '%s/topk_label_%s.%s.index' % (index_pt, topk_label_file_name, 'offline')
+    topk_label = DataUtil.load_matrix(topk_label_file_path, 'int')
 
-    # load rank features
-    feature_names = config.get('RANK', 'rank_features').split()
+    # load model features
+    feature_names = config.get('RANK', 'model_features').split()
     for feature_name in feature_names:
-        LogUtil.log('INFO', 'feature_name=%s' % feature_name)
-        rank_features_fp = '%s/rank_%s.%s.csv' % (config.get('DIRECTORY', 'dataset_pt'), feature_name, data_name)
-        LogUtil.log('INFO', 'rank_features_fp=%s' % rank_features_fp)
-        has_rank_features = isfile('%s' % rank_features_fp)
-        if has_rank_features:
-            LogUtil.log('INFO', 'has rank features, jump')
+        LogUtil.log('INFO', 'model_feature=%s' % feature_name)
+
+        featwheel_feature_file_path = '%s/featwheel_%s.%s.smat' % (config.get('DIRECTORY', 'dataset_pt'), feature_name, data_name)
+        LogUtil.log('INFO', 'featwheel_feature_path=%s' % featwheel_feature_file_path)
+        has_featwheel_features = isfile('%s' % featwheel_feature_file_path)
+        if has_featwheel_features:
+            LogUtil.log('INFO', 'has featwheel features, JUMP')
             continue
-        rank_features_f = open(rank_features_fp, 'w')
-        if 'offline' == data_name and 0 == feature_name.count('vote_'):
-            LogUtil.log('INFO', 'load_features_from_file')
-            features = load_features_from_file(config, feature_name, data_name, valid_index)
-        else:
-            LogUtil.log('INFO', 'load_matrix')
-            features = DataUtil.load_matrix('%s/%s.%s.csv' % (config.get('DIRECTORY', 'dataset_pt'), feature_name, data_name), 'float')
+        featwheel_feature_file = open(featwheel_feature_file_path, 'w')
+        features = DataUtil.load_matrix('%s/%s.%s.csv' % (config.get('DIRECTORY', 'dataset_pt'), feature_name, data_name), 'float')
 
-        assert len(topk_label_id) == len(features)
-        for line_id in range(len(topk_label_id)):
-            rank_features = [str(features[line_id][lid]) for lid in topk_label_id[line_id]]
-            rank_features_f.write('%s\n' % ','.join(rank_features))
+        featwheel_feature_file.write('%d %d\n' % (len(features) * topk, 1))
 
-        rank_features_f.close()
+        assert len(topk_label) == len(features)
+        for line_id in range(len(topk_label)):
+            rank_features = [str(features[line_id][lid]) for lid in topk_label[line_id]]
+            for lid in topk_label[line_id]:
+                Feature.save_feature([features[line_id][lid]], featwheel_feature_file)
+
+        featwheel_feature_file.close()
 
 
 if __name__ == '__main__':

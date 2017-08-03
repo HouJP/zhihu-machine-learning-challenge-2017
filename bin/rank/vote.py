523df943a39b3801b8d23ce267f204fb4b82805f
HouJP <houjp1992@gmail.com>
2017-08-03 21:09:49 +0800

    update vote

--- bin/rank/vote.py
+++ bin/rank/vote.py
@@ -7,27 +7,33 @@
 
 import sys
 import ConfigParser
+import hashlib
 from ..text_cnn.data_helpers import parse_feature_vec
 from ..utils import DataUtil, LogUtil
 from ..text_cnn.data_helpers import load_labels_from_file
 
 
 def vote(config, argv):
-    dataset_name = argv[0]
+    data_name = argv[0]
+
     dataset_pt = config.get('DIRECTORY', 'dataset_pt')
     index_pt = config.get('DIRECTORY', 'index_pt')
     class_num = config.getint('DATA_ATTRIBUTE', 'class_num')
     topk = config.getint('RANK', 'topk')
-    vote_features = config.get('RANK', 'vote_features').split()
-    vote_features_f = [open('%s/%s.%s.csv' % (dataset_pt, fn, dataset_name), 'r') for fn in vote_features]
-    topk_class_index_f = open('%s/%s.%s.index' % (index_pt, config.get('RANK', 'topk_class_index'), dataset_name), 'w')
-    LogUtil.log('INFO', 'vote_features=%s' % str(vote_features))
-    LogUtil.log('INFO', 'topk_class_index_f=%s' % str(topk_class_index_f))
+
+    vote_feature_names = config.get('RANK', 'vote_features').split()
+    vote_feature_files = [open('%s/%s.%s.csv' % (dataset_pt, fn, data_name), 'r') for fn in vote_feature_names]
+
+    topk_label_file_name = hashlib.md5('|'.join(vote_feature_names)).hexdigest()
+    topk_label_file = open('%s/topk_label_%s.%s.index' % (index_pt, topk_label_file_name, data_name), 'w')
+
+    LogUtil.log('INFO', 'vote_feature_names=%s' % str(vote_feature_names))
+    LogUtil.log('INFO', 'topk_label_file_name=%s' % str('%s/topk_label_%s.%s.index' % (index_pt, topk_label_file_name, data_name)))
 
     while True:
         aggregator = [0.] * class_num
         eof = False
-        for f in vote_features_f:
+        for f in vote_feature_files:
             line = f.readline()
             if '' == line:
                 eof = True
@@ -40,10 +46,10 @@ def vote(config, argv):
 
         #print aggregator
         topk_ids = [kv[0] for kv in sorted(enumerate(aggregator), key=lambda x: x[1], reverse=True)[:topk]]
-        topk_class_index_f.write('%s\n' % ' '.join([str(n) for n in topk_ids]))
+        topk_label_file.write('%s\n' % ' '.join([str(n) for n in topk_ids]))
 
-    topk_class_index_f.close()
-    for f in vote_features_f:
+    topk_label_file.close()
+    for f in vote_feature_files:
         f.close()
     analyze_vote(config, [])
 
@@ -60,22 +66,25 @@ def analyze_vote(config, argv):
 
     # load topk ids
     index_pt = config.get('DIRECTORY', 'index_pt')
-    topk_class_index_fp = '%s/%s.%s.index' % (index_pt, config.get('RANK', 'topk_class_index'), 'offline')
-    topk_label_id = DataUtil.load_matrix(topk_class_index_fp, 'int')
+    vote_feature_names = config.get('RANK', 'vote_features').split()
+    topk_label_file_name = hashlib.md5('|'.join(vote_feature_names)).hexdigest()
+    topk_label_file_path = '%s/topk_label_%s.%s.index' % (index_pt, topk_label_file_name, 'offline')
+    topk_label = DataUtil.load_matrix(topk_label_file_path, 'int')
 
     total_labels = 0
     right_labels = 0
 
     topk = config.getint('RANK', 'topk')
     class_num = config.getint('DATA_ATTRIBUTE', 'class_num')
-    assert len(valid_label_id) == len(topk_label_id)
-    for line_id in range(len(topk_label_id)):
+    assert len(valid_label_id) == len(topk_label)
+    for line_id in range(len(topk_label)):
         for class_id in range(class_num):
             if 1 == valid_label_id[line_id][class_id]:
                 total_labels += 1
-        for lid in topk_label_id[line_id]:
+        for lid in topk_label[line_id]:
             if 1 == valid_label_id[line_id][lid]:
                 right_labels += 1
+
     LogUtil.log('INFO', 'topk=%d, recall=%s%%' % (topk, str(100. * right_labels / total_labels)))
 
 

6e492c09e00cd321e181d01b363c8e0b1ef3e15b
HouJP <houjp1992@gmail.com>
2017-08-12 12:29:47 +0800

    update rank data generator

--- bin/rank/rankgbm/rank_data.py
+++ bin/rank/rankgbm/rank_data.py
@@ -151,10 +151,11 @@ def generate_online(config, argv):
     # pair_feature_names = config.get('RANK', 'pair_features').split()
 
     # load feature matrix
+    will_save = ('True' == config.get('RANK', 'will_save'))
     online_features = Feature.load_all(config.get('DIRECTORY', 'dataset_pt'),
                                        all_feature_names,
                                        'online',
-                                       False)
+                                       will_save)
 
     # load labels
     online_labels_file_path = '%s/featwheel_vote_%d_%s.%s.label' % (config.get('DIRECTORY', 'label_pt'),
